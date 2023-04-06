a2e67c0c790955df245c022fa61c3d37528eb37e
brunoais <brunoaiss@gmail.com>
2023-04-06 10:38:08 +0100

    Add ability to reloading modules automatically
    I use this a lot to test my own code and changes to tuxedo's codebase
    I'm sharing it in the hopes it will be useful for everybody

---
+++ reloadModules.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+systemctl stop tccd $(cat "$DIR"/.extra_restart 2> /dev/null)
+
+sleep 0.5
+
+for module in $(tac "$DIR"/modules.order) ; do
+	rmmod $(echo "$module" | sed -E 's/^.*\/([^/]+?)\.ko$/\1/');
+done;
+for module in $(cat "$DIR"/modules.order) ; do
+	insmod "$module";
+done;
+
+systemctl start tccd $(cat "$DIR"/.extra_restart 2> /dev/null)
